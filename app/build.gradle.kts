3689066905f8c882703a733d1f97051bf0cd1a6c
CloudYunLight <LearnCS777@gmail.com>
2025-04-04 20:42:09 +0800

    v0.1.3，添加FFMPEG依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,8 @@ dependencies {
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // https://mvnrepository.com/artifact/com.arthenica/ffmpeg-kit-full
+    implementation("com.arthenica:ffmpeg-kit-full:6.0.LTS")
+
 }
